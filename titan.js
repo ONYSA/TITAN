c431dea7984920301242eefd9660dbded08780b8
ONYSA <38070442+ONYSA@users.noreply.github.com>
2018-04-04 16:07:15 +0300

    TOB20180404AH

--- titan.js
+++ titan.js
@@ -2,4 +2,4 @@ const Discord = require("discord.js");
 const titanxyz = new Discord.Client();
 const prefix = '.';
 
-titanxyz.login('NDMwMzA4NzA4Njc4MTA3MTM2.DaOTyg._k-a033Ltiegn5kPHz4atTcoDEo')
+titanxyz.login(process.env.HTOKEN);
