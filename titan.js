da6e158fbfe6be433ec663ac1116aa1502a9c01f
Dzhemesyuk Artem <zziger07@gmail.com>
2018-04-04 17:18:58 +0300

    TOB20180404AD2

--- titan.js
+++ titan.js
@@ -6,17 +6,20 @@ function clear (channel) {
     channel.bulkDelete(98).then(messages => {
         if (messages.size === 98) {
             clear(channel);
+        } else {
+            channel.send(`Удалил все сообщения!`).then((msg) => {msg.delete(3000);});
         }
     })
 }
 
-function clear_count (channel, count, count_all) {
-    
+function clear_count (channel, count, count_all = 0) {
     if (count > 100) {
+        count_all = count_all + 100;
         channel.bulkDelete(100).then(() => {clear(channel, count-100, count_all)});
     } else {
-        channel.bulkDelete(count).then(() => {
-            message.channel.send(`Удалил ${count_all} сообщений!`).then((msg) => {msg.delete(3000);});
+        channel.bulkDelete(count).then(() => {            
+            count_all = count_all + count;
+            channel.send(`Удалил ${count_all} сообщений!`).then((msg) => {msg.delete(3000);});
         });
     }
 }
@@ -45,7 +48,7 @@ titanxyz.on('message', async (message) => {
     if (command === 'clear') {
         if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply('Ошибка');
         if (!args[0]) return message.reply('Ошибка');
-        clear_count(message.channel, parseInt(args[0])+1, parseInt(args[0])+1);
+        clear_count(message.channel, parseInt(args[0])+1);
     }
     
     if (command === 'clear_all') {
