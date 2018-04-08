e187278551605a54f8ce1a9df2492c19686a6900
ONYSA <38070442+ONYSA@users.noreply.github.com>
2018-04-09 00:11:50 +0300

    TOB20180409I003

--- titan.js
+++ titan.js
@@ -13,7 +13,7 @@ function clear (channel) {
         if (messages.size === 99) {
             clear(channel);
         } else {
-            channel.send(`del. ${count_all} ${declOfNum(count_all, ['сообщение','сообщения','сообщений'])} done.`).then((msg) => {msg.delete(3000);});
+            channel.send(`done.`).then((msg) => {msg.delete(3000);});
         }
     })
 }
