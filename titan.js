4a8e2638e668ee1ca47c349f2b626309fb152ea4
ONYSA <38070442+ONYSA@users.noreply.github.com>
2018-04-15 10:10:54 +0300

    TOB20180415I001

--- titan.js
+++ titan.js
@@ -24,7 +24,7 @@ function clear_count (channel, count, count_all = 0) {
         channel.bulkDelete(100).then(() => {clear(channel, count-100, count_all)});
     } else {
         channel.bulkDelete(count).then(() => {            
-            count_all = count_all + count;
+            count_all = count_all + 1;
             channel.send(`del. ${count_all} ${declOfNum(count_all, ['сообщение','сообщения','сообщений'])}.`).then((msg) => {msg.delete(3000);});
         });
     }
